10f3cd5cc460b82673a0711861f31fad3ced5a2e
k-mabbott <kmabbott9@gmail.com>
2023-12-21 10:25:49 -0800

    comment

--- core/views.py
+++ core/views.py
@@ -7,6 +7,7 @@ from item.models import Category, Item
 from .forms import SignupForm
 # Create your views here.
 
+# Index route
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
