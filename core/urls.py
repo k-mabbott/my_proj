833e63895e0f1e4584d7d8adca69cfab2944eeba
k-mabbott <kmabbott9@gmail.com>
2023-12-27 10:03:40 -0800

    Clean up

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 from .forms import LoginForm
 
+
 app_name = 'core'
+
 
 urlpatterns = [
     path('', views.index, name='index'),
